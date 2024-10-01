2f5708a5749e9d11d453e7bd4732c9e7991b530b
Dom <100814585+LeoDomGit@users.noreply.github.com>
2024-10-01 10:59:45 +0700

    Commit

---
+++ app/Http/Controllers/RolesController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Roles;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class RolesController extends BaseCrudController
+{
+
+    public function __construct()
+    {
+        $this->model = Roles::class;
+    }
+        /**
+     * Display a listing of the resource.
+     */
+
+    protected function validateRequest(Request $request)
+    {
+        return $request->validate([
+            'name' => 'required|string|unique:roles,name',
+        ]);
+    }
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $roles= Roles::all();
+        return Inertia::render('Roles/Index', ['roles'=>$roles]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Roles $roles)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Roles $roles)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Roles $roles,$id)
+    {
+        $role  = Roles::find($id);
+        if(!$role){
+            return response()->json(['check'=>false,'msg'=>'Không tìm thấy mã loại người dùng']);
+        }
+        $role->delete();
+        return response()->json(['check'=>true,'data'=>$this->model::all()]);
+    }
+}
