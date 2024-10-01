76d483bd46ba760df880bd4f687dbdf1a6524be4
Dom <100814585+LeoDomGit@users.noreply.github.com>
2024-10-01 10:01:58 +0700

    Commit

--- config/filesystems.php
+++ config/filesystems.php
@@ -56,7 +56,11 @@ return [
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => false,
         ],
-
+        
+        'app_backup' => [
+            'driver' => 'local',
+            'root' => storage_path('app/backup'),
+        ],
     ],
 
     /*
