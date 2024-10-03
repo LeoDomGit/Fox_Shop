92d518c4f1c7e6896034f70486f1a2f7b60e7105
hoanguser <137585402+hoanguser@users.noreply.github.com>
2024-10-03 10:32:43 +0700

    test

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,9 @@ use App\Http\Controllers\RolesController;
 use App\Models\Roles;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\PermissionController;
 Route::get('/', function () {
     return view('welcome');
 });
 Route::resource('/roles', RolesController::class);
+Route::resource('/permissions', PermissionController::class);
